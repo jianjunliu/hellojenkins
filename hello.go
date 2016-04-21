5aa909d348a809b4eeba1aba6dae84026ce7447e
jianjunliu <liujianjun6@qq.com>
2016-04-21 14:47:40 +0800

    3th push from dev

--- hello.go
+++ hello.go
@@ -12,4 +12,5 @@ func main() {
 	fmt.Println("this code origin from dev branch")
 	fmt.Println("new push from dev branch")
 	fmt.Println("2nd push from dev branch")
+	fmt.Println("3th push from dev branch")
 }
